8fd9d27a099e5df664f174aca14e8dc5438237fb
Francis Plaza <francis@francis.ph>
2013-11-12 12:33:30 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReliefHaiyan::Application.config.secret_token = '3964866e8f0f5b1b9076ebf1579d9b2a64d2f12ccade23690a3d18e1808b80b56862c6c2553731faa09801d07fb13be0a02e53233f7d6125b0a0cc1ceb757d99'
