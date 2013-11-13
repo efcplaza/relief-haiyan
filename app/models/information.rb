5ca4ed13895a28ed19e93abb2866f44f89b21ee6
Francis Plaza <francis@francis.ph>
2013-11-12 23:00:00 -0500

    hot fix change comments to addl_info in information model

--- app/models/information.rb
+++ app/models/information.rb
@@ -1,5 +1,5 @@
 class Information < ActiveRecord::Base
-  attr_accessible :city, :comments, :contact_email, :contact_person, :contact_phone, :first_name, :last_name, :latest_condition, :mobile_signal, :province, :street_one, :street_two, :urgent_needs, :with_relief_goods
+  attr_accessible :city, :addl_info, :contact_email, :contact_person, :contact_phone, :first_name, :last_name, :latest_condition, :mobile_signal, :province, :street_one, :street_two, :urgent_needs, :with_relief_goods
   
   has_many :comments
 end
