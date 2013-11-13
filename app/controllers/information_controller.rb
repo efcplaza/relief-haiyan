0fe49ef881f86dbfc42a7b881e7518ce06013c66
Francis Plaza <francis@francis.ph>
2013-11-12 19:57:29 -0500

    should now be able to add information

--- app/controllers/information_controller.rb
+++ app/controllers/information_controller.rb
@@ -1,11 +1,16 @@
 class InformationController < ApplicationController
   
+  def index
+  end
+  
   def new
     @info = Information.new
   end
   
   def create
-    @info = Information.create(params[:info])
+    @info = Information.create(params[:information])
   end
   
+  def show
+  end
 end
